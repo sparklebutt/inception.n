f2cfe40ec0c63c5dc44575779ba5a3188fd6f3aa
sparklebutt <arandomsanti@gmail.com>
2025-03-21 17:34:00 +0200

    fix: replaced more values with .env variables nginx.conf

--- inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-sed -i "s|\${CRT_PATH}|${CRT_PATH}|g" /etc/nginx/nginx.conf.template
-sed -i "s|\${KEY_PATH}|${KEY_PATH}|g" /etc/nginx/nginx.conf.template
+sed -i "s|\${CRT_PATH}|${CRT_PATH}|g" /etc/nginx/nginx.conf
+sed -i "s|\${KEY_PATH}|${KEY_PATH}|g" /etc/nginx/nginx.conf
+sed -i "s|\${DOMAIN_NAME}|${DOMAIN_NAME}|g" /etc/nginx/nginx.conf
+sed -i "s|\${WORDPRESS_PORT}|${WORDPRESS_PORT}|g" /etc/nginx/nginx.conf
 
 nginx -g "daemon off;" -c /etc/nginx/nginx.conf
